c47ad75ba777a2a692b53507070f329e42205ea2
seifmuhammedd <seifamuhammed2@gmail.com>
2025-03-10 23:14:49 +0200

    added carousels from owl carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,19 +2,72 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ProductsService } from '../../core/services/products.service';
 import { Iproduct } from '../../core/interfaces/iproduct';
 import { Subscription } from 'rxjs';
+import { CategoriesService } from '../../core/services/categories.service';
+import { ICategory } from '../../core/interfaces/icategory';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit , OnDestroy {
 
   private readonly _ProductsService = inject(ProductsService)
+  private readonly _CategoriesService = inject(CategoriesService)
+
+
   productsData !: Iproduct[]
+  categoriesData !: ICategory[]
   productsSub !: Subscription
+  categoriesSub !: Subscription
+
+  categoriesSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      },
+      1100: {
+        items: 6
+      }
+    },
+    nav: false
+  }
+  mainSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    items: 1,
+    nav: false
+  }
+
+
   ngOnInit(): void {
     this.productsSub = this._ProductsService.getAllProducts().subscribe({
       next : (res) => {
@@ -22,9 +75,17 @@ export class HomeComponent implements OnInit , OnDestroy {
       },
       error : (error) => {console.log(error)}
     })
+
+    this.categoriesSub = this._CategoriesService.getALLCategories().subscribe({
+      next : (res) => {
+        this.categoriesData = res.data
+      },
+      error : (error) => {console.log(error)}
+    })
   }
 
   ngOnDestroy(): void {
     this.productsSub?.unsubscribe()
+    this.categoriesSub?.unsubscribe()
   }
 }
