d06bc69cc866fda5d16f064b55b6989e105b0ef7
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-07 15:13:55 +0200

    guard terminal error solved

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,15 +1,22 @@
-import { inject, Inject } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  
-  const _Router = inject(Router)
 
-  if(sessionStorage.getItem("token")){
+  const _Router = inject(Router);
+  const _PLATFORM_ID = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(_PLATFORM_ID)) {
+    if (sessionStorage.getItem('token')) {
+      return true;
+    } else {
+      _Router.navigate(['/login']);
+      return false;
+    }
+  } else if (isPlatformServer(_PLATFORM_ID)) {
     return true;
-  }else{
-    _Router.navigate([ "/login" ])
+  } else {
     return false;
   }
-  
 };
